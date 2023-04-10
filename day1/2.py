465f667603bc30c4091a56bf9994429245aa362e
MohamedAsif08 <mohamed.asif.2k23@gmail.com>
2023-04-10 18:06:18 +0530

    day1

---
+++ day1/2.py
@@ -0,0 +1,6 @@
+print("-"*20)
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+print("-"*20)
+# String Concatenation
+print("Hello"+" World!")
+print("-"*20)
